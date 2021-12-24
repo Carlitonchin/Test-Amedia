a09bf82a44452babf46d91d596393811fd76ba31
Carlitonchin <carlozalejandro98@gmail.com>
2021-12-24 01:24:46 +0100

    voy a borrar la base de datos, commit de respaldo

---
+++ Data/ContextSeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_Crud_Carlos_Arrieta.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task createBase(ApplicationDbContext context) 
+        {
+            context.tRol.Add(new Models.tRol() { txt_desc = "Administrador" });
+            context.tRol.Add(new Models.tRol() { txt_desc = "Visitante" });
+            context.tGenero.Add(new Models.tGenero() { txt_desc = "Sin Genero" });
+            await context.SaveChangesAsync();
+            }
+    }
+}
